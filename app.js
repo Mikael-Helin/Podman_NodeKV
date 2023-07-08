bfe62593bad2f4cd6d264418883c13c256a5237d
Mikael Helin <mikaelhelin@yahoo.com>
2023-07-08 13:14:02 +0000

    clean up save

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url')
 const sqlite3 = require('sqlite3').verbose();
 const hostname = '0.0.0.0';
 const port = 80;
@@ -11,7 +12,118 @@ const db = new sqlite3.Database('/app/data/kvstore.db', (err) => {
   console.log('Connected to the kvstore.db database.');
 });
 
+const db_insert = (key, value, res) => {
+  db.run(`INSERT INTO kvstore(key, value, created, updated, active) VALUES(?, ?, ?, ?, ?)`,
+    [key, value, Date.now(), Date.now(), true],
+    function(err) {
+      let msg;
+      if (err) {
+        msg = `ERROR: Failed to insert data: ${err.message}`;
+        res.writeHead(500);
+        res.end(JSON.stringify({msg}));
+        return;
+      }
+
+      msg = 'OK: Insert successful';
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({key, value, msg}));
+  });
+};
+
+const db_update = (key, value, res) => {
+  db.run(`UPDATE kvstore SET value = ?, updated = ?, active = ? WHERE key = ?`,
+    [value, Date.now(), true, key],
+    function(err) {
+      let msg;
+      if (err) {
+        msg = `ERROR: Failed to insert data: ${err.message}`;
+        res.writeHead(500);
+        res.end(JSON.stringify({msg}));
+        return;
+      }
+
+      msg = 'OK: Update successful'
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({key, value, msg}));
+  });
+};
+
+const db_inactivate = (key, res) => {
+  db.run(`UPDATE kvstore SET updated = ?, active = ? WHERE key = ?`,
+    [Date.now(), false, key],
+    function(err) {
+      let msg;
+      if (err) {
+        msg = 'ERROR: Failed to inactivate: ${err.message}'
+        res.writeHead(500);
+        res.end(JSON.stringify({msg}));
+        return;
+      }
+
+      msg = 'OK: Inactivated'
+      res.writeHead(200, {'Content-Type': 'application/text'});
+      res.end(JSON.stringify({key, msg}));
+  });
+};
+
+const db_select_active = (key, res) => {
+  db.get(`SELECT value FROM kvstore WHERE key = ?, active = ?`,
+    [key, true],
+    function(err, row) {
+      let msg;
+      if (err) {
+        msg = `ERROR: Failed to select data: ${err.message}`
+        res.writeHead(500);
+        res.end(JSON.stringify({msg}));
+        return;
+      }
+      if (!row) {
+        msg = 'OK: Active key not found'
+        res.writeHead(404);
+        res.end(JSON.stringify({msg}));
+        return
+      }
+
+    const value = row.value
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({key, value}));
+  });
+};
+
 const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const method = req.method;
+
+  // Handle PUT requests to /store/:key
+  if (method === 'PUT' && path.startsWith('/store/')) {
+    const key = path.slice('/store/'.length);
+
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      const value = JSON.parse(body).value;
+      db_insert(key, value, res);
+        #db.run(`INSERT OR REPLACE INTO kvstore(key, value, created, updated, active) VALUES(?, ?, ?, ?, ?)`,
+        #[key, value, Date.now(), Date.now(), true],
+        #function(err) {
+        #  if (err) {
+        #    res.writeHead(500);
+        #    res.end(`Failed to insert data: ${err.message}`);
+        #    return;
+        #  }
+
+        # res.writeHead(200, {'Content-Type': 'application/json'});
+        # res.end(JSON.stringify({key, value}));
+      });
+    });
+    return;
+  }
+
+
+  // Existing code for index page...
   db.serialize(() => {
     db.all(`SELECT * FROM kvstore`, (err, rows) => {
       if (err) { console.error(err.message); }
